4fdb303897dd3e5cb140874a1ca5390c772cf5d7
DoubleMM02 <mutahirsulehri@gmail.com>
2024-07-29 12:03:12 -0400

    Assignment 4 - JavaScript
    Commit 12

--- assignments/a4/a4p1/main.js
+++ assignments/a4/a4p1/main.js
@@ -34,7 +34,7 @@ function result() {
     const weight = `${Math.round(300*0.0714286)} stone`;
     const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
     newStory = newStory.replaceAll('94 fahrenheit', temperature);
-
+    newStory = newStory.replaceAll('300 pounds', weight);
   }
 
   story.textContent = ;
