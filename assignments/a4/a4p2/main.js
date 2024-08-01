ad24a5b12ca5cc28acea7d2a26465e07c42fc922
DoubleMM02 <mutahirsulehri@gmail.com>
2024-08-01 17:14:44 -0400

    Assignment 4 - JavaScript
    Commit 25

--- assignments/a4/a4p2/main.js
+++ assignments/a4/a4p2/main.js
@@ -5,7 +5,7 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const imageFiles = ["pic1.jpg, pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"]
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 
 /* Declaring the alternative text for each image file */
 
@@ -13,7 +13,7 @@ const alts = {
     'pic1.jpg' : 'Closeup of the human eye',
     'pic2.jpg' : 'A Rock that looks like a wave',
     'pic3.jpg' : 'Purple and white flowers',
-    'pic4.jpg' : 'A painting of a pharos',
+    'pic4.jpg' : 'A painting of pharos',
     'pic5.jpg' : 'A Large moth on a leaf'
 }
 
@@ -38,4 +38,9 @@ btn.addEventListener('click', () => {
         btn.setAttribute('class','light');
         btn.textContent = 'Lighten';
         overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-    })
+    } else {
+        btn.setAttribute('class','dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+      }
+    });
