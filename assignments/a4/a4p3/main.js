131c4c15a677e6410b771bc3ce2c685fc160dd59
DoubleMM02 <mutahirsulehri@gmail.com>
2024-08-01 17:51:16 -0400

    Assignment 4 - JavaScript
    Commit 40

--- assignments/a4/a4p3/main.js
+++ assignments/a4/a4p3/main.js
@@ -93,6 +93,12 @@ collisionDetect() {
   for (const ball of balls) {
     if (this !== ball) {
       const dx = this.x - ball.x;
-      const dx = this.x - ball.x;
+      const dy = this.y - ball.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
 
+      if (distance < this.size + ball.size){
+        ball.color = this.color = randomRGB();
+      }
     }
+  }
+}
