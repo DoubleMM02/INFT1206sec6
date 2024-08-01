67c0be2e495190a07f6b934d181204ddaf39fefc
DoubleMM02 <mutahirsulehri@gmail.com>
2024-08-01 16:37:45 -0400

    Assignment 4 - JavaScript
    Commit 24

--- assignments/a4/a4p2/main.js
+++ assignments/a4/a4p2/main.js
@@ -32,6 +32,10 @@ newImage.addEventListener('click', e => {
 
 /* Wiring up the Darken/Lighten button */
 
-btn.addEventListener('click, () =>' {
+btn.addEventListener('click', () => {
     const btnClass = btn.getAttribute('class');
-})
+    if (btnClass === 'dark') {
+        btn.setAttribute('class','light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    })
