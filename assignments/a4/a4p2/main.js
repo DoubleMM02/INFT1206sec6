bd73608bb06e8e9cb16ba7d2b994ab4a8706d826
DoubleMM02 <mutahirsulehri@gmail.com>
2024-08-01 16:16:02 -0400

    Assignment 4 - JavaScript
    Commit 16

--- assignments/a4/a4p2/main.js
+++ assignments/a4/a4p2/main.js
@@ -1,10 +1,11 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+
+const imageFiles = ["pic1.jpg, pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"]
 
 /* Declaring the alternative text for each image file */
 
