9570475e54096ec9a22bb3c4d7dc832054e0d691
DoubleMM02 <mutahirsulehri@gmail.com>
2024-08-01 18:18:59 -0400

    Assignment 4 - JavaScript
    Commit 42

--- assignments/a4/a4p3/main.js
+++ assignments/a4/a4p3/main.js
@@ -21,11 +21,11 @@ function randomRGB() {
 class Ball {
   constructor(x, y, velX, velY, color, size) {
     this.x = x;
-    this.x = y;
-    this.x = velX;
-    this.x = velY;
-    this.x = color;
-    this.x = size;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
   }
 
   draw() {
@@ -34,27 +34,41 @@ class Ball {
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
   }
-  
+
   update() {
     if (this.x + this.size >= width) {
       this.velX = -Math.abs(this.velX);
     }
-    
+
     if (this.x - this.size <= 0) {
-    this.velX = Math.abs(this.velX);
+      this.velX = Math.abs(this.velX);
     }
-    
+
     if (this.y + this.size >= height) {
-    this.velY = -Math.abs(this.velY);
-  }
-  
-  if (this.y - this.size <= 0) {
-    this.velX = -Math.abs(this.velY);
+      this.velY = -Math.abs(this.velY);
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = Math.abs(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
   }
 
-  this.x += this.velX;
-  this.x += this.velY;
+  collisionDetect() {
+    for (const ball of balls) {
+      if (!(this === ball)) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
 
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
 }
 
 const balls = [];
@@ -76,30 +90,16 @@ while (balls.length < 25) {
 }
 
 function loop() {
-  ctx.fillStyle = "rgb(0 0 0 / 25%)";
-  ctx.fillRect = (0, 0, width, height);
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
 
   for (const ball of balls) {
     ball.draw();
     ball.update();
-    ball.collisionDetect
+    ball.collisionDetect();
   }
 
   requestAnimationFrame(loop);
 }
 
 loop();
-
-collisionDetect() {
-  for (const ball of balls) {
-    if (this !== ball) {
-      const dx = this.x - ball.x;
-      const dy = this.y - ball.y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-
-      if (distance < this.size + ball.size){
-        ball.color = this.color = randomRGB();
-      }
-    }
-  }
-}
