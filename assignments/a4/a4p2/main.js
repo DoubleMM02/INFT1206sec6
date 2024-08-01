581c076710c149585c7649f838efe30769d9bfe2
DoubleMM02 <mutahirsulehri@gmail.com>
2024-08-01 16:31:19 -0400

    Assignment 4 - JavaScript
    Commit 19

--- assignments/a4/a4p2/main.js
+++ assignments/a4/a4p2/main.js
@@ -6,12 +6,16 @@ const overlay = document.querySelector('.overlay');
 /* Declaring the array of image filenames */
 
 const imageFiles = ["pic1.jpg, pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"]
+
+/* Declaring the alternative text for each image file */
+
 const alts = {
     'pic1.jpg' : 'Closeup of the human eye',
     'pic2.jpg' : 'A Rock that looks like a wave',
+    'pic3.jpg' : 'Purple and white flowers',
+    'pic4.jpg' : 'A painting of a pharos',
+    'pic5.jpg' : 'A Large moth on a leaf'
 }
-
-/* Declaring the alternative text for each image file */
 
 /* Looping through images */
 
